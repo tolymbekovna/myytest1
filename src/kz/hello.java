9db036cb6056f334b85bae2188327347efc59c5f
Tolganay Tolymmbek <tolymbekovna@gmail.com>
2017-10-03 15:37:24 +0600

    2-commit

--- src/kz/hello.java
+++ src/kz/hello.java
@@ -3,5 +3,7 @@ package kz;
 public class hello {
     public static void main(String[] args){
         System.out.println("Hello");
+        System.out.println("zert j maksaty");
+        System.out.println("GITHUB uyrenu");
     }
 }
